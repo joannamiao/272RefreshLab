9ec9849188550749c3f762075e3b0223f325764d
Joanna Miao <joannam1@andrew.cmu.edu>
2021-02-03 18:16:51 -0800

    added child model references, validations, scopes, and method

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,4 +3,14 @@ class Child < ApplicationRecord
     has_many :tasks, :through => :chores
 
     validates_presence_of :first_name, :last_name
+
+    scope :alphabetical, -> { order('last_name', 'first_name') }
+    scope :active, -> { where(active: true) }
+
+    def name 
+        full_name = self.first_name + " "+ self.last_name
+        return full_name
+    end
+
+    
 end
